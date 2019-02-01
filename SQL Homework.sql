24eb06ed21892ee5f3c01d6d0a5efd09c10a5a0d
William Bonney <43503785+Wbonney90@users.noreply.github.com>
2019-02-01 12:17:45 -0500

    Delete SQL Homework.sql

--- SQL Homework.sql
+++
@@ -1,8 +0,0 @@
-#use sakila
-#select first_name, last_name from actor
-SELECT CONCAT(first_name, ' ', last_name) AS 'Actor Name' FROM actor;
-select * from actor 
-	where first_name = "joe"
-
-select * from actor
-	where last_name contains %gen%
